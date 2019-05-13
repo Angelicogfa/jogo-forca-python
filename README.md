a4746b13e75e7c902a6f33df69fd8729b5bdb080
Guilherme Fernando Angelico <angelicogfa@gmail.com>
2019-05-13 00:41:18 -0300

    correção storage file

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 # Jogoo da forca em python
 
+Este projeto tem como finalidade efetuar o laboratorio do curso de analise de Python fundamentos para analise de dados.
+
 Para rodar o jodo basta rodar o seguinte comando no diretorio principal
 
 ```
--- storage/storage_file.py
+++ storage/storage_file.py
@@ -1,14 +1,18 @@
 import random
+import os
 
 class storage_file:
 
    def __init__ (self):
-       with open('C:\\Users\\guilherme.angelico\\Documents\\Estudos\\python\\jogo_forca\\storage\\palavras.txt', 'r') as file:
+
+       basepath = os.path.abspath(os.path.dirname(__file__))
+       path = os.path.join(basepath, './palavras.txt')
+
+       with open(path, 'r') as file:
            self.__palavras__ = file.readlines()
            file.close()
 
    def get_palavra(self):
         posicao = random.randrange(0, len(self.__palavras__))
         palavra = self.__palavras__[posicao]
-        print('Palavra storage: {}'.format(palavra))
         return palavra.split(';')
